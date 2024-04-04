f39560d17a0f76dff7e517624fda7c8e630167fd
bellesouza <127145848+bellesouza@users.noreply.github.com>
2024-04-04 17:38:15 -0300

    Add files via upload

---
+++ 2_estruturas/script.js
@@ -0,0 +1,28 @@
+//exemplo de funcoes bult-in -- funções nativas
+//pronpt:
+let nome = prompt("Qual é o seu nome?");
+
+// Alert:
+
+let mensagem = "Seja bem vindo(a)  ${nome} " ;
+alert(mensagem);
+// Math.x
+/* tipos de math.x 
+/*math.max() - retorna o maior numero de uma lista de argumentos
+/*math.min() - retorna o menor numero de uma lista de argumentos
+/*math.random() - retorna um numero aleatorio entre 0 e 1
+/*math.pow() - retorna a base elevada ao expoente
+/*math.abs() - retorna o valor absoluto de um numero
+
+/*math.sqrt() - retorna a raiz quadrada de um numero
+/*etc......
+*/
+
+let maioirNumero = Math.max(10, 20, 30, 40, 50);
+let menorNumero = Math.min(10, 20, 30, 40, 50);
+console.log(nome);
+console.log('maioirNumero: $(maioirNumero)');
+console.log('menorNumero:  $(menorNumero)');
+
+
+// console.log0
